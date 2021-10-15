9fe768cf80d4601b9f367ecd983127315f6a12d4
MiltonLiquinchana <liquinchana@hotmail.es>
2021-10-15 15:24:05 -0500

    se elimino libreria en desuso

--- 01_ServiceFileData/src/main/java/com/mflq/galery/repository/TypeFileRepository.java
+++ 01_ServiceFileData/src/main/java/com/mflq/galery/repository/TypeFileRepository.java
@@ -1,7 +1,5 @@
 package com.mflq.galery.repository;
 
-import java.util.List;
-
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
