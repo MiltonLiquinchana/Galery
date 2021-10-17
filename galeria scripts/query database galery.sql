621c8ec1ef55a292219009d4ab2501024bd9f0cc
MiltonLiquinchana <liquinchana@hotmail.es>
2021-10-16 22:00:13 -0500

    nuevo script agrupa por tipo

--- galeria scripts/query database galery.sql
+++ galeria scripts/query database galery.sql
@@ -24,5 +24,5 @@ FOREIGN KEY(FK_TYPEFILE) REFERENCES TypeFile(PK_TYPEFILE)
 select * from TypeFile order by PK_TYPEFILE asc;
 select * from FileData order by PK_FILEDATA asc;
 
-select * from FileData
+select * from FileData group by FK_TYPEFILE
 
