39aea6b4716941da50df46433a94efabc1e2abaa
MiltonLiquinchana <liquinchana@hotmail.es>
2021-11-22 15:37:27 -0500

    Comentario forzado borrado

--- 01_ServiceFileData/src/main/java/com/mflq/galery/controllers/FileDataController.java
+++ 01_ServiceFileData/src/main/java/com/mflq/galery/controllers/FileDataController.java
@@ -23,7 +23,7 @@ public class FileDataController {
 	@GetMapping("listfiledata")
 	public ResponseEntity<List<FileData>> listFileData() {
 		return new ResponseEntity<>(fileDataService.findAllFileData(), HttpStatus.OK);
-		/*Comentario forsozo*/
+		/*Comentario forsozo borrado*/
 	}
 
 
